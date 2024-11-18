0d642285df17fa2029b0e7cd619921fe0109892c
howellea <howelleddy@gmail.com>
2024-11-17 23:54:31 -0600

    made cookie crack button works for change image

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,43 @@ const fortune = [
     "It is time to choose a new path!",
     "Keep those you love close!"
 ];
+const button = document.getElementById('getFortune');
+const WholeCookie = document.getElementById('whole-cookie');
+const BrokenCookie =document.getElementById('broken-cookie')
+
+// gets a fortune at ramdom from the furtune array
 getRandomFortune = () => {
     const randomIndex = Math.floor(Math.random() * fortune.length);
     return fortune[randomIndex];
 }
-document.getElementById('getFortune').addEventListener('click', () => {
+button.addEventListener('click', () => {
     const fortuneMessage = getRandomFortune();
     document.getElementById('fortuneDisplay').innerText = fortuneMessage;
 });
+
+let isWholeCookie =true;
+button.addEventListener('click',() => {
+
+    // Fade out the whole-cookie
+
+    isWholeCookie =false
+    WholeCookie.src = './assets/images/broken-cookie.png';
+    WholeCookie.alt = 'A broken fortune cookie';
+    console.log('The cookie broke!');
+
+
+ // Wait for the fade-out transition, then change the source from whole-cookie to broken-cookie
+    setTimeout(() => {
+    if (isWholeCookie) {
+        WholeCooke.src = '../images/whole-cookie.png';
+    } else {
+        WholeCooke.src = '../images/broken-cookie.png';
+    }
+    isWholeCookie = ! isWholeCookie;
+    WholeCooke.classList.remove('hidden'); // Fade the image back in
+ }, 500); // Match this to the CSS transition duration   
+});
+
 const changeLanguage = () => {
     const languageSelect = document.getElementById('language-select');
     const title = document.getElementById('title');
@@ -34,21 +63,3 @@ const changeLanguage = () => {
             title.textContent = 'Crack the Fortune Cookie';
     }
 };
-
-const WholeCooke = document.getElementById('whole-cookie');
-const button = document.getElementById('ChangeCookie');
-let isWholeCookie =true;
-button.removeEventListener('click',() => {
-    WholeCooke.classList.add('hidden'); // Fade out the whole-cookie
-
- // Wait for the fade-out transition, then change the source from whole-cookie to broken-cookie
-    setTimeout(() => {
-    if (isWholeCookie) {
-        WholeCooke.src = '../images/whole-cookie.png';
-    } else {
-        WholeCooke.src = '../images/broken-cookie.png';
-    }
-    isWholeCookie = ! isWholeCookie;
-    WholeCooke.classList.remove('hidden'); // Fade the image back in
- }, 500); // Match this to the CSS transition duration   
-});
