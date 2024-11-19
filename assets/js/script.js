f888d4c4522e2435342601de563d5bcbb9090a64
howellea <howelleddy@gmail.com>
2024-11-18 18:58:33 -0600

    added mobile functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,3 +81,49 @@ const changeLanguage = () => {
             title.textContent = 'Crack the Fortune Cookie';
     }
 };
+ // Language functions
+function setLanguage(language) {
+    localStorage.setItem('selectedLanguage', language);
+}
+function getLanguage() {
+    return localStorage.getItem('selectedLanguage') || 'en'; // Default to English
+}
+// Fortune functions
+function addFortune(fortune) {
+    let fortunes = JSON.parse(localStorage.getItem('fortuneHistory')) || [];
+    fortunes.push(fortune);
+    localStorage.setItem('fortuneHistory', JSON.stringify(fortunes));
+}
+function getFortuneHistory() {
+    return JSON.parse(localStorage.getItem('fortuneHistory')) || [];
+}
+// Event listeners
+document.getElementById('languageSelector').addEventListener('change', function() {
+    setLanguage(this.value);
+});
+document.getElementById('generateFortune').addEventListener('click', function() {
+    const fortunes = [
+        "You will have a great day!",
+        "You are in for a big surprise!",
+        "Your future is filled with uncertainty!",
+        "It is time to choose a new path!",
+        "Keep those you love close!"
+    ];
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    document.getElementById('fortuneDisplay').innerText = randomFortune;
+    addFortune(randomFortune);
+});
+// Load initial settings
+document.getElementById('languageSelector').value = getLanguage();
+
+React
+
+Reply
+
+
+
+
+
+
+
+
